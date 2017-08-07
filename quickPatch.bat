19086ca00eabf880e1b7a6cbc92bd7a1939f67db
Nikolai Ommundsen <post@niikoo.net>
2017-08-07 15:31:45 +0200

    Bugfixes

--- quickPatch.bat
+++ quickPatch.bat
@@ -1,10 +1,12 @@
 @echo off
 echo.
 echo "Committing patch and incrementing version"
+copy README.md dist\README.md
+copy LICENSE dist\LICENSE
 git add .
 git commit -m "Bugfixes"
 npm version patch
-cd dist/
+cd dist\
 npm version patch
 cd ..
 npm run test
